5c8d0d510898ba34c516352f236a8e83a6adbdd1
prashantugale84 <prashantugale007@gmail.com>
2024-03-07 14:59:40 +0530

    by Raju Ugale

--- DemoProject/src/com/model/A.java
+++ DemoProject/src/com/model/A.java
@@ -5,6 +5,10 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
 	}
+	
+	public void m1() {
+		System.out.println("Hello Method m1");
+	}
 
 	public void m2(){
 		System.out.println("Hello Method m2");
